5111cb8c9a27809ee39b7148a29aac8a8173dbb4
Hoang Tuan Dung <tuandungyz09@gmail.com>
2026-06-08 15:05:21 +0700

    [Dung] sửa file env

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -3,17 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHA_ZRc20T7TreP55OKcwoHSjlS4xYRIE",
-  authDomain: "mln-111-group2.firebaseapp.com",
-  databaseURL: "https://mln-111-group2-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "mln-111-group2",
-  storageBucket: "mln-111-group2.firebasestorage.app",
-  messagingSenderId: "724066399446",
-  appId: "1:724066399446:web:2b2c01b924e8e9efb74577",
-  measurementId: "G-QXZD7TTLT4"
+  apiKey: import.meta.env.VITE_GEMINI_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
-export const adminUid = "zN4N69XJuscHAbvFnsteIWgRhKR2"
+export const adminUid = import.meta.env.VITE_ADMINUID;
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
